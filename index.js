85e4c2dfd92ef0e2e877546fd0f2542ac0be212e
Joe Siew <joesiew98@gmail.com>
2019-05-14 23:39:20 +0800

    added database

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const config = require('config')
 //Production
 const helmet = require('helmet')
 const compression = require('compression')
-//mongodb+srv://Joe:<password>@project-caelum-rli3m.mongodb.net/test?retryWrites=true
 //Database
 //const db = config.get('db');
-const db = "mongodb+srv://Joe:<joesiew>@project-caelum-rli3m.mongodb.net/ProjectCaelum"
+const db = 'mongodb+srv://Joe:joesiew@project-caelum-rli3m.mongodb.net/test?retryWrites=true'
 mongoose.connect(db, { useNewUrlParser :true } )
 .then(() => console.log(`Connected to MongoDB at ${db}...`))
 .catch(err => console.error(`Could not connect to MongoDB at ${db}...`))
